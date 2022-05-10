5eb1add065282857774b3f030f15fb702d5be6b3
Danilo <danilo.sants@live.com>
2022-05-10 18:18:52 +0100

    construcao segunda tela

--- MeuPrimeiroAppIos/ViewController.swift
+++ MeuPrimeiroAppIos/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtLogin: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
+        
+
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func openAction(_ sender: Any) {
+        
+        if (txtLogin.text == "danilo" && txtPassword.text == "1234"){
+            performSegue(withIdentifier: "tela2", sender: self)
+            
+        }
+            else
+        {
+            print("Wrong user or password")
+            
+        }
+        
+    }
+    
 }
 
